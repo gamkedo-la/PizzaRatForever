8f0810351bcd941e7fc446d945658fd078bac49f
Steve Palley <spall03@gmail.com>
2015-10-25 19:38:38 -0700

    first commit

---
+++ Assets/Scripts/PizzaRatController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PizzaRatController : MonoBehaviour {
+
+	public float movementSpeed;
+	public float rotationSpeed;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody>(); 
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//move PizzaRat when buttons are pressed
+	void FixedUpdate(){
+
+		float moveForward = Input.GetAxis("Vertical") * movementSpeed;
+		float rotation = Input.GetAxis ("Horizontal") * rotationSpeed;
+		
+		transform.Rotate (0, rotation, 0);
+		transform.Translate(-moveForward, 0, 0);
+		
+	}
+}
