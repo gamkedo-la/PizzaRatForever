84444a79643fad41502280104d5accb189a4b370
Steve Palley <spall03@gmail.com>
2015-11-08 17:55:17 -0800

    starting on shoe dropping mechanic

---
+++ Assets/Scripts/ShoeController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShoeController : MonoBehaviour {
+
+	public float shoeKillTime = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision col){
+
+		if (col.gameObject.CompareTag("Player")){
+
+			Debug.Log ("Rat squashed!");
+
+		}
+		else if (col.gameObject.CompareTag("Pizza")){
+
+			Debug.Log ("Pizza stepped on!");
+
+		}
+		else{
+
+			Invoke ("KillShoe", shoeKillTime);
+
+		}
+	
+	}
+
+	void KillShoe(){
+
+		Destroy (this.gameObject);
+
+	}
+}
