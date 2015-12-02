9707dcfd6b1c0d080f80ce1634a6e1a1cbc86102
Steve Palley <spall03@gmail.com>
2015-12-01 18:56:59 -0800

    basic title and credits screens added

---
+++ Assets/Scripts/TitleFunctions.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleFunctions : MonoBehaviour {
+
+	public GameObject mainCanvas;
+	public GameObject creditsCanvas;
+
+	public void Start(){
+
+		mainCanvas.SetActive(true);
+		creditsCanvas.SetActive(false);
+
+	}
+
+	public void ToggleCredits(){
+
+		creditsCanvas.SetActive(mainCanvas.activeSelf);
+		mainCanvas.SetActive(mainCanvas.activeSelf==false); //invert
+
+	}
+
+	public void StartGame(){
+
+		Application.LoadLevel("Level 1");
+
+	}
+}
